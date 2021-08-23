a62b760a2728b1f740b11a5f54b9a425a6f8fed2
freelancera42 <freelancera42@gmail.com>
2021-08-23 09:13:03 +0000

    Use Simple template

---
+++ LearningFlask/HelloFlask/views.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flask import Flask, render_template
+from HelloFlask import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
